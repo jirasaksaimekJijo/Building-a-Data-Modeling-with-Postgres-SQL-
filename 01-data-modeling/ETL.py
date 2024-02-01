d58f6f08d7b3b842dda0debcae0e97b597a6dcab
Jirasak Saimek <116647943+jirasaksaimekJijo@users.noreply.github.com>
2024-02-02 02:49:00 +0700

    Update ETL.py

--- 01-data-modeling/ETL.py
+++ 01-data-modeling/ETL.py
@@ -22,7 +22,6 @@ def get_files(filepath: str) -> List[str]:
 
     return all_files
 
-
 def process(cur, conn, filepath):
     # Get list of files from filepath
     all_files = get_files(filepath)
@@ -33,17 +32,27 @@ def process(cur, conn, filepath):
             for each in data:
                 # Print some sample data
                 
-                if each["type"] == "IssueCommentEvent":
+                if each["type"] == "PushEvent":
                     print(
                         each["id"], 
                         each["type"],
                         each["actor"]["id"],
                         each["actor"]["login"],
+                        each["actor"]["display_login"],
+                        each["actor"]["gravatar_id"],
+                        each["actor"]["url"],
+                        each["actor"]["avatar_url"],
                         each["repo"]["id"],
                         each["repo"]["name"],
+                        each["repo"]["url"],
                         each["created_at"],
-                        each["payload"]["issue"]["url"],
-                    )
+                    )  
+                    if "payload" in each:
+                        # Accessing "push_id" from the first commit in the commits array
+                        if "commits" in each["payload"] and len(each["payload"]["commits"]) > 0:
+                            each["payload"]["commits"][0]["sha"]
+                            each["payload"]["commits"][0]["author"]["email"]
+                            each["payload"]["commits"][0]["message"]
                 else:
                     print(
                         each["id"], 
@@ -59,8 +68,12 @@ def process(cur, conn, filepath):
                 insert_statement = f"""
                     INSERT INTO actors (
                         id,
-                        login
-                    ) VALUES ({each["actor"]["id"]}, '{each["actor"]["login"]}')
+                        login,
+                        display_login,
+                        gravatar_id,
+                        url,
+                        avatar_url
+                    ) VALUES ({each["actor"]["id"]}, '{each["actor"]["login"]}', '{each["actor"]["display_login"]}', '{each["actor"]["gravatar_id"]}', '{each["actor"]["url"]}', '{each["actor"]["avatar_url"]}')
                     ON CONFLICT (id) DO NOTHING
                 """
                 # print(insert_statement)
@@ -75,7 +88,42 @@ def process(cur, conn, filepath):
                     ) VALUES ('{each["id"]}', '{each["type"]}', '{each["actor"]["id"]}')
                     ON CONFLICT (id) DO NOTHING
                 """
-                # print(insert_statement)
+                cur.execute(insert_statement)
+
+                insert_statement = f"""
+                    INSERT INTO repo (
+                        id,
+                        name,
+                        url,
+                        actor_id
+                    ) VALUES (
+                        '{each["repo"]["id"]}',
+                        '{each["repo"]["name"]}',
+                        '{each["repo"]["url"]}',
+                        '{each["actor"]["id"]}'
+                    ) ON CONFLICT (id) DO NOTHING
+                """
+
+                commit_sha = each["payload"]["commits"][0]["sha"] if ("payload" in each and "commits" in each["payload"] and each["payload"]["commits"]) else ''
+                payload_size = each["payload"]["size"] if ("payload" in each and "size" in each["payload"]) else ''
+                payload_distinct_size = each["payload"]["distinct_size"] if ("payload" in each and "distinct_size" in each["payload"]) else ''
+                actor_id = each["actor"]["id"] if ("actor" in each and "id" in each["actor"]) else ''
+
+                cur.execute(insert_statement)
+
+                insert_statement = f"""
+                    INSERT INTO payload (
+                        push_id,
+                        size,
+                        distinct_size,
+                        actor_id
+                    ) VALUES (
+                        '{commit_sha}',
+                        '{payload_size}',
+                        '{payload_distinct_size}',
+                        '{actor_id}'
+                    ) ON CONFLICT (push_id) DO NOTHING
+                """
                 cur.execute(insert_statement)
 
                 conn.commit()
