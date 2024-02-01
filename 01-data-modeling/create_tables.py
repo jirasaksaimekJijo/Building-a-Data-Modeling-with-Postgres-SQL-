d4be9f5141b89f8f49ba56ce84e5f6bf2cc07fc4
Jirasak Saimek <116647943+jirasaksaimekJijo@users.noreply.github.com>
2024-02-02 02:49:22 +0700

    Update create_tables.py

--- 01-data-modeling/create_tables.py
+++ 01-data-modeling/create_tables.py
@@ -1,7 +1,5 @@
 from typing import NewType
-
 import psycopg2
-
 
 PostgresCursor = NewType("PostgresCursor", psycopg2.extensions.cursor)
 PostgresConn = NewType("PostgresConn", psycopg2.extensions.connection)
@@ -11,17 +9,40 @@ table_drop_actors = "DROP TABLE IF EXISTS actors"
 
 table_create_actors = """
     CREATE TABLE IF NOT EXISTS actors (
-        id int,
+        id int PRIMARY KEY,
         login text,
-        PRIMARY KEY(id)
+        display_login text,
+        gravatar_id text,
+        url text,
+        avatar_url text
     )
 """
+
 table_create_events = """
     CREATE TABLE IF NOT EXISTS events (
-        id text,
+        id text PRIMARY KEY,
         type text,
         actor_id int,
-        PRIMARY KEY(id),
+        CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id)
+    )
+"""
+
+table_create_repo = """
+    CREATE TABLE IF NOT EXISTS repo (
+        id text PRIMARY KEY,
+        name text,
+        url text,
+        actor_id int,
+        CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id)
+    )
+"""
+
+table_create_payload = """
+    CREATE TABLE IF NOT EXISTS payload (
+        push_id text PRIMARY KEY,
+        size text,
+        distinct_size text,
+        actor_id int,
         CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id)
     )
 """
@@ -29,50 +50,37 @@ table_create_events = """
 create_table_queries = [
     table_create_actors,
     table_create_events,
+    table_create_repo,
+    table_create_payload,
 ]
 drop_table_queries = [
-    table_drop_events,
-    table_drop_actors,
+    table_create_actors,
+    table_create_events,
+    table_create_repo,
+    table_create_payload,
 ]
 
 
 def drop_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
-    """
-    Drops each table using the queries in `drop_table_queries` list.
-    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur: PostgresCursor, conn: PostgresConn) -> None:
-    """
-    Creates each table using the queries in `create_table_queries` list.
-    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
-    """
-    - Drops (if exists) and Creates the sparkify database.
-    - Establishes connection with the sparkify database and gets
-    cursor to it.
-    - Drops all the tables.
-    - Creates all tables needed.
-    - Finally, closes the connection.
-    """
     conn = psycopg2.connect(
         "host=127.0.0.1 dbname=postgres user=postgres password=postgres"
     )
     cur = conn.cursor()
-
     drop_tables(cur, conn)
     create_tables(cur, conn)
-
     conn.close()
-
 
 if __name__ == "__main__":
     main()
